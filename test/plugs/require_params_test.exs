d52776297ba1b6099981b6df7deb008dd8c5a3fb
Kamil Lelonek <squixy.sln@gmail.com>
2017-04-07 17:55:55 +0200

    Test Plug itself with custom options

---
+++ test/plugs/require_params_test.exs
@@ -0,0 +1,28 @@
+defmodule PlugExample.Plugs.RequireParamsTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  alias PlugExample.Plugs.RequireParams
+
+  @opts RequireParams.init([params: ["s"]])
+
+  describe "failure" do
+    test "should reject a request with incomplete params" do
+      conn = :get |> conn("/") |> RequireParams.call(@opts)
+
+      assert conn.state     == :sent
+      assert conn.status    == 400
+      assert conn.resp_body == "Query params: [\"s\"] must be provided!"
+    end
+  end
+
+  describe "success" do
+    test "should accept a request with all required params" do
+      conn = :get |> conn("/?s=elixir") |> RequireParams.call(@opts)
+
+      assert conn.state     == :sent
+      assert conn.status    == 200
+      assert conn.resp_body == "Query params were: [\"s\"]."
+    end
+  end
+end
